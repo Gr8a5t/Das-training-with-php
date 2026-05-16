b8dd42272080b80edd5d8edf54c2bd35dae672f4
Great Onweazu <greatonweazu48@gmail.com>
2026-05-16 18:39:10 +0100

    yh🔥

--- Linked-List/index.php
+++ Linked-List/index.php
@@ -18,3 +18,26 @@ class Node
 $first = new Node(10);
 echo $first->data;
 ?>
+
+<p> Now to Create with Multiple Nodes </p>
+<?php
+
+class Nod
+{
+    public $data;
+    public $next;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+        $this->next = null;
+    }
+}
+
+$first = new Nod(10);
+$first->next = new Nod(20);
+$first->next->next = new Nod(30);
+echo $first->data.'<br>';
+echo $first->next->data.'<br>';
+echo $first->next->next->data.'<br>';
+?>
